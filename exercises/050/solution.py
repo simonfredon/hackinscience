df52cdfbd0a96aab7ff60ded98cbec8408d44d35
SIMON FREDON <simon.fredon@sciencespo.fr>
2015-09-22 15:25:08 +0200

    exo50_10

--- exercises/050/solution.py
+++ exercises/050/solution.py
@@ -6,13 +6,10 @@ Created on Tue Sep 22 15:11:48 2015
 """
 
 a = 0
-for i in range(0, 999):
+for i in range(0, 1000):
     if i % 3 == 0:
         a += i
-
-b = 0
-for i in range(0, 999):
     if i % 5 == 0:
-        b += i
+        a += i
 
-print(a + b)
+print(a)

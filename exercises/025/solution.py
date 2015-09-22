6b260e735a96e85e67f880bcb70af827969dda21
SIMON FREDON <simon.fredon@sciencespo.fr>
2015-09-22 14:42:15 +0200

    exo25

---
+++ exercises/025/solution.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 14:29:59 2015
+
+@author: simonfredon
+"""
+
+import datetime
+
+print (datetime.datetime.now().strftime("Today is %Y-%m-%d and it is %H:%M:%S"))

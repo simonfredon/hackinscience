aecdf23ba3c21752e29670467295565e30c8fc5b
SIMON FREDON <simon.fredon@sciencespo.fr>
2015-09-22 10:39:30 +0200

    exo7

---
+++ exercises/006/solution.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 10:19:37 2015
+
+@author: simonfredon
+"""
+
---
+++ exercises/007/solution.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 10:38:16 2015
+
+@author: simonfredon
+"""
+
+L = ["Hello World", 42]
+
+print(L)

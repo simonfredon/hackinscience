55dfb14cea295e76475e00804d9738153d5aaaa9
SIMON FREDON <simon.fredon@sciencespo.fr>
2015-09-22 11:01:01 +0200

    exo9_2

--- exercises/009/solution.py
+++ exercises/009/solution.py
@@ -14,4 +14,4 @@ endlessly debates this alarming chain of events, the Supreme\
 Chancellor has secretly dispatched two Jedi Knights, the guardians of\
 peace and justice in the galaxy, to settle the conflict"""
 
-len(phantom_menace)
+print(len(phantom_menace))

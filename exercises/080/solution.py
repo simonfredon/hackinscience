e05ec6985390b7b6d995a92ea22ddea320c306ad
SIMON FREDON <simon.fredon@sciencespo.fr>
2015-09-22 19:04:07 +0200

    exo80

---
+++ exercises/080/solution.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 19:02:51 2015
+
+@author: simonfredon
+"""
+
+import string
+
+a = string.ascii_lowercase
+
+b = 1
+for i in range(0, 25):
+    for j in range (b, 26):
+        print(a[i] + a[j])
+b = b + 1

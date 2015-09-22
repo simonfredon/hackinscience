7ed49b07cc95d368afe4d5a369e2c4dd9f42e3a7
SIMON FREDON <simon.fredon@sciencespo.fr>
2015-09-22 14:16:59 +0200

    exo14

---
+++ exercises/014/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 11:31:44 2015
+
+@author: simonfredon
+"""
+
+import sys
+
+if len(sys.argv)>1:
+    print(sys.argv[1])
+else:
+    print("PARAM")

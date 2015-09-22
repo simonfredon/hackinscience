9f049583d065691b71b1e49c1b41e8296d9ec306
SIMON FREDON <simon.fredon@sciencespo.fr>
2015-09-22 10:56:26 +0200

    exo9

---
+++ exercises/009/solution.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 10:54:40 2015
+
+@author: simonfredon
+"""
+
+phantom_menace = """Turmoil has engulfed the Galactic Republic. The\
+taxation of trade routes to outlying star systems is in\
+dispute. Hoping to resolve the matter with a blockade of deadly\
+battleships, the greedy Trade Federation has stopped all shipping to\
+the small planet of Naboo. While the congress of the Republic\
+endlessly debates this alarming chain of events, the Supreme\
+Chancellor has secretly dispatched two Jedi Knights, the guardians of\
+peace and justice in the galaxy, to settle the conflict"""
+ 
+len(phantom_menace)

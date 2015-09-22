1e38ee1a093f1db358251021acf5525bfc866dc5
SIMON FREDON <simon.fredon@sciencespo.fr>
2015-09-22 14:29:26 +0200

    exo20

---
+++ exercises/020/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 14:28:47 2015
+
+@author: simonfredon
+"""
+
+import sys
+
+if len(sys.argv) > 2:
+    print(int(sys.argv[1]) - int(sys.argv[2]))
+else:
+    print("usage: python3 solution.py OP1 OP2")

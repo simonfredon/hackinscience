0806e4417cfb1b19ec0782cfb4157d808e628115
SIMON FREDON <simon.fredon@sciencespo.fr>
2015-09-22 14:23:39 +0200

    exo19

---
+++ exercises/019/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 14:22:50 2015
+
+@author: simonfredon
+"""
+
+import sys
+
+if len(sys.argv) > 2:
+    print(sys.argv[1] + sys.argv[2])
+else:
+    print("usage: python3 solution.py PARAM")

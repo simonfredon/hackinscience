cf4d8dce03b12ec52831820d1a3bd73374bd2983
SIMON FREDON <simon.fredon@sciencespo.fr>
2015-09-22 16:27:42 +0200

    exo60

---
+++ exercises/060/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 15:27:28 2015
+
+@author: simonfredon
+"""
+
+word="abcdefghijklmnopqrstuvwxyz"
+
+for i in range(0,26):
+    for j in range(0,26):
+        print(word[i], word[j])

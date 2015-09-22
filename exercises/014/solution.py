63c3e5d0eb9f9e914e196cbcfd3a2dae4f3907bd
SIMON FREDON <simon.fredon@sciencespo.fr>
2015-09-22 14:21:37 +0200

    exo14_4

--- exercises/014/solution.py
+++ exercises/014/solution.py
@@ -7,7 +7,7 @@ Created on Tue Sep 22 11:31:44 2015
 
 import sys
 
-if len(sys.argv)> 1:
+if len(sys.argv) > 1:
     print(sys.argv[1])
 else:
-    print("PARAM")
+    print("usage: python3 solution.py PARAM")

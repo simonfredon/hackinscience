b6088e2f47df053007727fd7461dc00d076e594a
Simon Fredon <simon.fredon@sciencespo.fr>
2015-09-23 17:55:29 +0200

    exo328

---
+++ exercises/328/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 23 17:35:26 2015
+
+@author: simonfredon
+"""
+
+
+def mul(var):
+    res = 1
+    for i in var:
+        res = res * i
+    return res

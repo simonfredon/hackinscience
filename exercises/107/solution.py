7263da00389b8919be59a6fbf5a827ffe054d542
Simon Fredon <simon.fredon@sciencespo.fr>
2015-09-23 10:11:15 +0200

    exo107

---
+++ exercises/107/solution.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 23 10:05:12 2015
+
+@author: simonfredon
+"""
+
+from operator import *
+
+
+def select_student(l, tr):
+    a = len(l)
+    acc = list()
+    ref = list()
+    for i in range(0, a):
+        if l[i][1] >= tr:
+            acc.append(l[i])
+        else:
+            ref.append(l[i])
+    res = {'Accepted': sorted(acc, key=itemgetter(1), reverse=True),
+            'Refused': sorted(ref, key=itemgetter(1))}
+    return res

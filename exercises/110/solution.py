ce04c468f324471d591391d91903f337bc2323ac
Simon Fredon <simon.fredon@sciencespo.fr>
2015-09-23 10:30:25 +0200

    exo110

--- exercises/110/solution.py
+++ exercises/110/solution.py
@@ -7,31 +7,30 @@ Created on Wed Sep 23 10:15:34 2015
 
 import sys
 
-command = ['+', '-', '\*', '//', '%', '^']
+command = ['+', '-', '*', '/', '%', '^']
 
-if len(sys.argv) > 3:
-    def op(a, b, c):
-        if isinstance(a, int):
-            if isinstance(c, int):
-                if b in command:
-                    if b == '+':
-                        d = a + b
-                    elif b == '-':
-                        d = a - b
-                    elif b == '\*':
-                        d = a * b
-                    elif b == '//':
-                        d = a / b
-                    elif b == '%':
-                        d = a % b
-                    else:
-                        d = a ** b
-                    print(d)
-                else:
-                    print('input error')
-            else:
-                print('input error')
+if len(sys.argv) == 4:
+    b = sys.argv[2]
+    try:
+        a = int(sys.argv[1])
+        c = int(sys.argv[3])
+        if b in command:
+            if b == '+':
+                d = a + c
+            elif b == '-':
+                d = a - c
+            elif b == '*':
+                d = a * c
+            elif b == '/':
+                d = a / c
+            elif b == '%':
+                d = a % c
+            elif b == '^':
+                d = a ** c
+            print(d)
         else:
             print('input error')
+    except:
+        print("input error")
 else:
     print('usage: ./solution.py a_number (an_operator +-*/%^) a_number')

84736dfc6dba6ad22d39fc939f2819520a69a3a9
Simon Fredon <simon.fredon@sciencespo.fr>
2015-09-23 18:23:29 +0200

    exo215

--- exercises/215/solution.py
+++ exercises/215/solution.py
@@ -5,12 +5,28 @@ Created on Wed Sep 23 15:50:28 2015
 @author: simonfredon
 """
 
-import isprime
 
-from numpy import binary_repr
+def is_prime(num):
+    if num == 1:
+        return False
+    n = num ** 0.5
+    for i in list(range(2, (int(n) + 1))):
+        if num % i == 0:
+            return False
+    return True
+
+
+def dec2bin(d,nb=0):
+    if d==0:
+        b="0"
+    else:
+        b=""
+        while d!=0:
+            b="01"[d&1]+b
+            d=d>>1
+    return b.zfill(nb)
 
 for i in range(222281, 222381):
-    x = binary_repr(i)
-    if isprime.is_prime(x):
+    x = dec2bin(i)
+    if is_prime(x):
         print(i) 
-

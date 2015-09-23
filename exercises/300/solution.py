59c566e4a36bc578b430d7a738d397b91aad938d
Simon Fredon <simon.fredon@sciencespo.fr>
2015-09-23 13:52:33 +0200

    exo300

---
+++ exercises/300/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 23 13:52:05 2015
+
+@author: simonfredon
+"""
+
+inpy = open("words.txt", "r")
+inprint = inpy.read()
+inpy.close()
+
+print(inprint)

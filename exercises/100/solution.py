cf25df7636e2a07f92d73b47159298f395566c71
Simon Fredon <simon.fredon@sciencespo.fr>
2015-09-23 09:56:37 +0200

    exo100

---
+++ exercises/100/solution.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 23 09:53:49 2015
+
+@author: simonfredon
+"""
+
+station = {
+    'address': 'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE) - 93170 BAGNOLET',
+    'number': 31705,
+    'latitude': 48.8645278209514,
+    'name': 'CHAMPEAUX (BAGNOLET)',
+    'longitude': 2.416170724425901
+}
+
+print('latitude', station['latitude'])
+print('longitude', station['longitude'])
+print('number', station['number'])
+print('name', station['name'])
+print('address', station['address'])

5438393ffdb92699cba6a64db99868de421fa53b
Simon Fredon <simon.fredon@sciencespo.fr>
2015-09-23 11:57:38 +0200

    exo203

--- exercises/203/solution.py
+++ exercises/203/solution.py
@@ -6,10 +6,10 @@ Created on Wed Sep 23 11:18:49 2015
 """
 
 
-def is_multiple(a):
-    b = int(len(a) / 2)
-    c = [0] * len(a)
-    for i in range(0, b):
-        c[(i * 2)] = a[i]
-        c[(i * 2) + 1] = a[i + (b)]
-    return c
+def perfect_shuffle(a):
+    la = int(len(a) / 2)
+    fin = [0] * len(a)
+    for i in range(0, la):
+        fin[(i * 2)] = a[i]
+        fin[(i * 2) + 1] = a[i + (la)]
+    return fin

c7d3885162a57a2db288fce2ceea51c4461bfe68
Simon Fredon <simon.fredon@sciencespo.fr>
2015-09-23 13:55:43 +0200

    exo320

--- exercises/320/solution.py
+++ exercises/320/solution.py
@@ -5,17 +5,17 @@ Created on Wed Sep 23 10:54:46 2015
 @author: pippo
 """
 import string
-alphy = list(string.ascii_lowercase)
-print(alphy)
+alph = list(string.ascii_lowercase)
+print(alph)
 inpy = open("words.txt", "r")
 inprint = inpy.read()
-lungo = len (inprint)
+long = len (inprint)
 count = [0] * 26
 for j in range(0, 26):
     for i in inprint:
-        if i == alphy[j]:
+        if i == alph[j]:
             count[j] += 1
 inpy.close()
 
 for j in range(0, 26):
-    print(alphy[j] + ':', count[j] / lungo)
+    print(alph[j] + ':', count[j] / long)

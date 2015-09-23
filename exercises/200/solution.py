e1743209bc85b3505b5a844f3cc287c8e8ce5bbf
Simon Fredon <simon.fredon@sciencespo.fr>
2015-09-23 10:44:14 +0200

    exo200

--- exercises/200/solution.py
+++ exercises/200/solution.py
@@ -7,15 +7,12 @@ Created on Wed Sep 23 10:31:30 2015
 
 
 def is_prime(num):
-    check = 0
-    x = True
-    for i in range(2, num - 1):
-        if num % i == 0:
-            check += 1
-    if check > 0:
-        x = False
+    n = num ** 0.5
+    if num == 0:
+        return False
     else:
-        x = True
-    if num == 1:
-        x = False
-    return x
+        for i in range(2, n + 1):
+            if num % i == 0:
+                return False
+            else:
+                return True

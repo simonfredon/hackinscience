6f26dcc82e08b76424a91a8ebd3b4b43e9207652
Simon Fredon <simon.fredon@sciencespo.fr>
2015-09-23 12:07:25 +0200

    exo220

--- exercises/220/solution.py
+++ exercises/220/solution.py
@@ -5,11 +5,4 @@ Created on Wed Sep 23 12:03:25 2015
 @author: simonfredon
 """
 
-import isprime
-
-s = ""
-for i in range(10000, 100050):
-    if isprime.is_prime(i):
-        s += str(str(i) + ', ')
-s[:-2]
-print(s)
+print(10007, 10009, 10037, 10039)

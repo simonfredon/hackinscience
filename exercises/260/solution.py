8d07ae493b53626f149667e388da90a59e1d8d4f
Simon Fredon <simon.fredon@sciencespo.fr>
2015-09-24 13:47:13 +0200

    exo331

--- exercises/260/solution.py
+++ exercises/260/solution.py
@@ -5,9 +5,21 @@ Created on Thu Sep 24 09:42:40 2015
 @author: simonfredon
 """
 
+import math
+import numpy
+
 
 def euclidean(a, b):
-    for i in a:
-        
-        d = ((u - x) ** 2 + (v - y) ** 2) ** 0.5
-    
+    d = ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** 0.5
+    return d
+
+
+def opt_euclidean(a, b):
+    d2 = math.pow(math.pow(b[0] - a[0], 2)  + math.pow(b[1] - a[1], 2), 0.5)
+    return d2
+
+
+def np_euclidean(a, b):
+    d3 = numpy.power(numpy.power(b[0] - a[0], 2)  + \
+    numpy.power(b[0] - a[0], 2), 0.5)
+    return d3

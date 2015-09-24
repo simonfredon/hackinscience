f68e13af712edff8d78c55398cfbbf1d7c641b54
Simon Fredon <simon.fredon@sciencespo.fr>
2015-09-24 17:30:25 +0200

    exo260

---
+++ exercises/455/solution.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 24 17:16:21 2015
+
+@author: simonfredon
+"""
+
+import string
+
+L = string.ascii_uppercase
+
+
+def gen_colors(x):
+    if x in range(0, 26):
+        print(L[:x])
+    else:
+        print(L)
+     
+    

7ef283abb5b81dbc52b686c4b3a5531c23b9326a
Simon Fredon <simon.fredon@sciencespo.fr>
2015-09-24 15:02:40 +0200

    exo250

--- exercises/250/solution.py
+++ exercises/250/solution.py
@@ -9,6 +9,7 @@ Created on Thu Sep 24 14:23:48 2015
 def draw_n_squares(n):
     li1 = []
     li2 = []
+    ciao = []
     carre1 = ('+---')
     carre2 = ('|   ')
     for i in range(0, n):
@@ -16,8 +17,9 @@ def draw_n_squares(n):
         li2.append(carre2)
     l1 = ''.join(li1)
     l2 = ''.join(li2)
-    ciao = (l1 + '+\n' + l2 + '|\n' + l1 + '+\n')
+    ciao = (l1 + '+\n' + l2 + '|\n')
     ciaone = []
     for i in range(0, n):
         ciaone.append(ciao)
+    ciaone.append(l1 + "+\n")
     return ''.join(ciaone)

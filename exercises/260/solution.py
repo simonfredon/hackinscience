469e6bddcd72008f1ddc1859733de9c8d1692186
Simon Fredon <simon.fredon@sciencespo.fr>
2015-09-24 15:15:14 +0200

    exo260

--- exercises/260/solution.py
+++ exercises/260/solution.py
@@ -18,9 +18,10 @@ def opt_euclidean(a, b):
     d2 = math.pow(math.pow(b[0] - a[0], 2) + math.pow(b[1] - a[1], 2), 0.5)
     return d2
 
-
 a = np.array(())
+
 b = np.array(())
+
 
 
 def np_euclidean(a, b):

04fb6177b6cbd6a43d0b0e0c936cb9fe2b0108db
Simon Fredon <simon.fredon@sciencespo.fr>
2015-09-25 11:07:29 +0200

    exo260

--- exercises/455/solution.py
+++ exercises/455/solution.py
@@ -6,18 +6,41 @@ Created on Thu Sep 24 17:16:21 2015
 """
 
 import string
+import random
 
-L = string.ascii_uppercase
+alpha = string.ascii_uppercase
 
 
 def gen_colors(x):
     if x in range(0, 26):
-        print(L[:x])
+        print(alpha[:x])
     else:
-        print(L)
+        print(alpha)
+
+
+def gen_code(x, alpha):
+    return ''.join(random.choice(alpha[:x]) for i in range(x))
+
+guess = ""
+
+
+def check_guess(guess, x, alpha):
+    if len(gen_code(x, alpha)) == len(guess):
+        if guess in alpha[:x]:
+            return True
+    return False
 
  
-def gen_code(x, b):
-    if i in b:
+def score_guess(guess, gen_code):
+    count1 = 0
+    count2 = 0
+    for i in range(guess):
+        if guess[i] == gen_code[i]:
+            count1 = count1 + 1
+        else:
+            if guess[i] in gen_code:
+                count2 = count2 + 1
+    return (count1, count2)
+
+
         
-    

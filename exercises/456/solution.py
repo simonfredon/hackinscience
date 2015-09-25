2a67a943d993472d4db0076f8c8eb85196a87c9d
Simon Fredon <simon.fredon@sciencespo.fr>
2015-09-25 19:31:42 +0200

    exo260

--- exercises/456/solution.py
+++ exercises/456/solution.py
@@ -10,19 +10,32 @@ from master_mind import *
 
 
 def solve_mind(x, code):
-    tries = list(itertools.combinations_with_replacement(x, len(code)))
-    for i in tries:
-        puttana = []
-        for j in i:
-            puttana.append(j)
-        i = puttana
-    for j in range(0, len(tries)):
-        a = random.choice(tries)
-        if a == code:
-            return (a, j)
-        else:
-            culo = score_guess(a, code)
-            for i in tries:
-                vaffan = score_guess(i, code)
-                if vaffan == culo:
-                    tries.remove(i)
+    tries = [''.join(x) for x in
+                itertools.combinations_with_replacement(x, len(code))]
+    guess_count = 1
+    while True:
+        guess = tries.pop(random.randint(0, len(tries) - 1))
+        if guess == code:
+            return (guess, guess_count)
+        guess_count += 1
+        score = score_guess(guess, code)
+        tries = [x for x in 
+                    tries if score_guess(x, code) == score]
+
+#    for i in tries:
+#        puttana = ""
+#        for j in i:
+#            puttana = puttana + j
+#        tries2.append(puttana)
+#    tries = tries2
+#    for j in range(0, len(tries)):
+#        a = random.choice(tries)
+#        print(a)
+#        if a == code:
+#            return (a, j)
+#        else:
+#            culo = score_guess(a, code)
+#            for i in tries:
+#                vaffan = score_guess(i, code)
+#                if vaffan == culo:
+#                    tries.remove(i)

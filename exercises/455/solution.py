a97c374eccbdcdcfdd250ff8e328650ffde621b2
Simon Fredon <simon.fredon@sciencespo.fr>
2015-09-25 14:59:24 +0200

    exo260

--- exercises/455/solution.py
+++ exercises/455/solution.py
@@ -37,7 +37,7 @@ def score_guess(guess, gen_code):
     for i, j in zip(guess, gen_code):
         if i == j:
                 count1 += 1
-                gen_code = gen_code.replace(j, "0")
+                gen_code = gen_code.replace(j, "0", 1)
         elif i in gen_code:
                 count2 += 1
                 gen_code = gen_code.replace(i, "0", 1)
